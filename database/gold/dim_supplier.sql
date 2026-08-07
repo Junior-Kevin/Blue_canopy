110cee4294e8af54372077f275c7775fbdd7e9a2
Junior <junioranalyst2021@gmail.com>
2026-08-07 18:44:09 +0300

    Update dim_supplier.sql

--- database/gold/dim_supplier.sql
+++ database/gold/dim_supplier.sql
@@ -1,21 +1,16 @@
-USE Blue_canopy;
-GO
-DROP TABLE IF EXISTS gold.dim_supplier;
-GO
+DROP TABLE IF EXISTS [gold].[dim_supplier];
 SELECT 
-       ROW_NUMBER() OVER(order by supplier_id)  supplier_sk
+ ROW_NUMBER() OVER(ORDER BY supplier_id,valid_from,valid_to) supplier_key
        ,[supplier_id]
+      ,[valid_from]
+      ,[valid_to]
       ,[supplier_name]
-	  ,[category]
       ,[contact_person]
       ,[phone]
-      ,REPLACE([email],'contact',
-	   LOWER(CONCAT(LEFT(supplier_name,1),
-	     SUBSTRING(supplier_name,CHARINDEX(' ',supplier_name)+1,1)))) email
-      ,[valid_from]
+      ,[email]
       ,[payment_terms]
       ,[lead_time_days]
+      ,[category]
       ,[tax_id]
-	  INTO gold.dim_supplier
-  FROM [Blue_canopy].[silver].[suppliers]
-  WHERE supplier_id NOT LIKE '%DUP' 
+INTO [gold].[dim_supplier]
+FROM [Blue_canopy].[silver].[suppliers]
