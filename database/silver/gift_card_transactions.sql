6c2fdf1926f49062572140b20aa3d027ce5a2686
Junior <junioranalyst2021@gmail.com>
2026-08-08 17:19:08 +0300

    Update gift_card_transactions.sql to include linked_transaction_id
    Modified the SQL query to include linked_transaction_id and adjusted the INTO clause.

--- database/silver/gift_card_transactions.sql
+++ database/silver/gift_card_transactions.sql
@@ -1,9 +1,11 @@
 DROP TABLE IF EXISTS silver.gift_card_transactions;
+GO
 SELECT 
        [transaction_id]
       ,[card_number]
-      ,CAST([date]AS DATE) date
-      ,CAST([amount] AS INT) amount 
-      ,[type]
-INTO silver.gift_card_transactions
+      ,CAST([date] AS DATE) date
+      ,CAST([amount] AS INT)amount 
+      ,SUBSTRING([type],1,CHARINDEX(',',type)-1) type
+	  ,SUBSTRING(type,CHARINDEX(',',type)+1,20) linked_transaction_id
+INTO gift_card_transactions
 FROM [Blue_canopy].[bronze].[gift_card_transactions_raw]
